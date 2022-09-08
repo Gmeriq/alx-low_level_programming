46acbe7a15efe973747f9eacfc217c3ce8dec02c
Nathan <frevnjengamungai@gmail.com>
2022-09-08 07:17:14 -0700

    Printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  *Desccription:main-Prints out a string to stdout.
  *Return:0 if success.
  */
-int main(void){
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
