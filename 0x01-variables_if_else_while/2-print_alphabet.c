281b99ce490422284eb91251002504e110256095
Nathan <frevnjengamungai@gmail.com>
2022-09-09 04:06:40 -0700

    Printing the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Description:main-Print alphabets in lowercase
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+char ch;
+ascii = ch - 32;
+putchar(The alphabets are,);
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch\n);
+}
+return (0);
+}
