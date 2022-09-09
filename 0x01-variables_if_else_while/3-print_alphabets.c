f67423db8abe2dc4af613b92ca0373387ef1d7b7
Nathan <frevnjengamungai@gmail.com>
2022-09-09 04:34:12 -0700

    Printing in uppper case and lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * Description:main- Printing alphabets in smallcase and uppercase
+ * Return: ALways 0 (success)
+ */
+int main(void)
+{
+char ch, CH;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch)
+}
+putchar('\n')
+for (CH = 'A'; CH <= 'Z'; CH++)
+{
+putchar(CH)
+}
+putchar('\n')
+return (0);
+}
+
