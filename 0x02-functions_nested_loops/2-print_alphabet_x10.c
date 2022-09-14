209ad879952c6e94ffe3c03fe09c24bd50572f3e
Nathan <frevnjengamungai@gmail.com>
2022-09-14 02:49:15 -0700

    Printing alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *Description:Print_alphabet-Printing all alphabets 10 times
+ *Description:Print_alphabet_x-Printing all alphabets 10 times
  */
 void print_alphabet_x10(void)
 {
 int num;
 int ch;
-for (num = 0; num <= 10; num++)
+for (num = 0; num < 10; num++)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {
