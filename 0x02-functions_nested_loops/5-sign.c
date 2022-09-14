9740b99523b34e3af299935f9cc98f25781e714d
Nathan <frevnjengamungai@gmail.com>
2022-09-14 06:27:14 -0700

    number sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,16 +9,16 @@ int print_sign(int n)
 if (n > 0)
 {
 return (1);
-putchar('+');
+putchar ('+');
 }
 else if (n == 0)
 {
 return (0);
-putchar('0');
+putchar ('0');
 }
 else
 {
-return (-1)
-putchar('-');
+return (-1);
+putchar ('-');
 }
 }
