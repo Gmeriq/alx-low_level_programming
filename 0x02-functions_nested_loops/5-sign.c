335222e3f89df75daaea701ee98c6f4c4d1a9ad6
Nathan <frevnjengamungai@gmail.com>
2022-09-14 04:29:12 -0700

    Checking number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ *Description:print_sign-Find the sign of a number
+ *Return: 1 when true 0 when false
+ *@n:is the variable
+ */
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+putchar('+');
+else if (n == 0)
+return (0);
+putchar('0');
+else
+return (-1)
+putchar('-');
+}
