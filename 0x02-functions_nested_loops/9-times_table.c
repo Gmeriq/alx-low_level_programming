e5068031b5943beb7602a78768cd1cc9f062583e
Nathan <frevnjengamungai@gmail.com>
2022-09-14 05:32:14 -0700

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,17 +9,17 @@ for (row = 0; row <= 9; row++)
 {
 putchar('0');
 putchar(',');
-putchar('-');
+putchar(' ');
 for (col = 0; col <= 9; col++)
 {
 num = (row * col);
 if ((num / 10) > 0)
 putchar((num / 10) > 0);
 else
-putchar('-');
+putchar(' ');
 if (col < 9)
 putchar(',');
-putchar('-');
+putchar(' ');
 }
 putchar('\n');
 }
