e2709a0bb5987ec3164076111463e788ff267cf8
Nathan <frevnjengamungai@gmail.com>
2022-09-14 05:25:41 -0700

    Multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *Description: times_table - Creating multiplication table
+ */
+void times_table(void)
+{
+int row, col, num;
+for (row = 0; row <= 9; row++)
+{
+putchar('0');
+putchar(',');
+putchar(' ');
+for (col = 0; col <= 9; col++)
+{
+num = (row * col);
+if ((num / 10) > 0)
+putchar((d / 10) > 0);
+else
+putchar('');
+if (col < 9)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+}
+}
