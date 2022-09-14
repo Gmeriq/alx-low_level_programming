c7d3dc875fb7f77dbb4a6a98dca69008c5a1ba51
Nathan <frevnjengamungai@gmail.com>
2022-09-14 07:11:11 -0700

    Sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *Description:main-print the sum of all multiples
+ *Return:Always 0 when successful
+ */
+int main(void)
+{
+int i, res;
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+res += i;
+}
+printf("%d\n", res);
+}
+return (0);
+}
