98763225398ed5d9f320ec39663fae8d4a02ba6f
Nathan <frevnjengamungai@gmail.com>
2022-09-15 05:07:58 -0700

    Checking for integer

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * Description: _isdigit-Check for intergers
+ * @c:Variable checked
+ * Return:1 when integer 0 when otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
+}
