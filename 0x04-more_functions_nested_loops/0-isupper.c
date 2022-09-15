dc2d7d44b2e13c2c968b47a4d52b57e41bfaf547
Nathan <frevnjengamungai@gmail.com>
2022-09-15 04:46:42 -0700

    Checking whether upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int_isupper-Check case of character
+ * @c: Character checked for status
+ * Return:1 when upper o when lower
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
