746123c46a423861fa81f9f2e038dfb05c8d28c4
Nathan <frevnjengamungai@gmail.com>
2022-09-19 06:57:54 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,15 +1,15 @@
 #include "main.h"
+
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
- */
-int _atoi(char *arr)
+*_atoi - Entry point
+*@s: input
+*Return: Always 0 (Success)
+*/
+int _atoi(char *s)
 {
-int i = 0, convert = 0;
-for (i = 0; arr[i] != '\0'; ++i)
-{
-convert = convert * 10 + arr[i] - '0';
-}
-return (convert);
+
+
+
+	return (0);
+
 }
