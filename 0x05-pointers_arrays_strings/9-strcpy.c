481c98461a8d8f79c1329bc9a360a7b65dd18b39
Nathan <frevnjengamungai@gmail.com>
2022-09-19 06:11:12 -0700

    Copying string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,13 @@
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
-for (; src[i] != '\0'; i++)
+if (src[i] != '\0')
 {
-dest[i] = src[i];
+for (i = 0; *(src + i) != '\0'; i++)
+{
+*(dest + i) = *(src + i);
 }
+}
+*(dest + i) = '\0';
 return (dest);
 }
