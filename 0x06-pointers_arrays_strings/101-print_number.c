1a34976f159a2490527e4013d3f4809723aeb838
Nathan <frevnjengamungai@gmail.com>
2022-09-22 05:37:26 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,22 +7,12 @@
  */
 void print_number(int n)
 {
-int pot_10 = 1, sign = 1, tmp = n;
-while (tmp / 10)
+if (n < 0)
 {
-pot_10 = pot_10 * 10;
-tmp = tmp / 10;
-}
-if (tmp < 0)
-{
-sign = sign * -1;
 putchar('-');
+n *= -1;
 }
-while (pot_10 > 0)
-{
-tmp = n / pot_10;
-putchar((tmp *sign)+'0');
-n = n - (tmp *pot_10);
-pot_10 /= 10;
-}
+if (n / 10)
+print_number(n / 10);
+putchar((n % 10) + '0');
 }
