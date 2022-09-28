7363064a2c58b0dfa8fa30e3fb046b0e9f66fca6
Nathan <frevnjengamungai@gmail.com>
2022-09-28 06:51:42 -0700

    Raising a number to a power using recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,13 +2,12 @@
 /**
  * _pow_recursion - x raised for the power of y
  * @x: Integer raised to the power of y
- * @y: the number of times x is raised
- * 
+ * @y: the number of times x is raised 
  * Return: the result of x^y or -1
  */
 int _pow_recursion(int x, int y)
 {
-if (y <0)
+if (y < 0)
 {
 return (-1);
 }
