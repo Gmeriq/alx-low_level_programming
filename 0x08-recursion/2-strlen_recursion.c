763a6ac3686f4d1f1a9eb87f622ca62097665ccb
Nathan <frevnjengamungai@gmail.com>
2022-09-28 05:02:23 -0700

    The length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of a string using recursion
+ * @s: string measured
+ * Return:Length of string when successful 0 when 
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+return (1 + _strlen_recursion(s + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_h
 #include <stdio.h>
+#include <string.h>
+int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif
