b6caccc6a3d768bda1902d42e904d34a9a227abf
Nathan <frevnjengamungai@gmail.com>
2022-09-30 02:54:27 -0700

    Print the number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,6 +7,14 @@
  */
 int main(int argc, char __attribute__ ((unused))*argv[])
 {
+int i;
+if (argc == 1)
 printf("%d", argc);
+else
+{
+for (i = 0; *argv; i++, argv++)
+;
+printf("%d\n", i - 1);
+}
 return (0);
 }
