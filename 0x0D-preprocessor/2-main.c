2461e05016ac8f82e7ff9ffc4f90aa636091fdf7
Nathan <frevnjengamungai@gmail.com>
2022-10-11 08:05:57 -0700

    Names the current file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * File: 2-main.c
+ * Desc: Prints the name of the current file
+ * Return: 0 on success
+ */
+int main()
+{
+printf ("The name on the file is %s\n", __FILE__);
+return (0);
+}
