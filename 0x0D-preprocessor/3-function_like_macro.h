856245b4688704d9dea2e6b659f2271760fd2df5
Nathan <frevnjengamungai@gmail.com>
2022-10-11 08:25:39 -0700

    Computes the absolute value of an int

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,9 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKEMACRO_H
-#include <stdlib.h>
 /**
  * File: 3-function_like_macro.h
  * Desc: A macro that returns the absolute
  *       value of an integer
  */
-#define ABS(x) abs(x)
+#define ABS(x) (((x) < 0) ? (-x) : (x))
 #endif
