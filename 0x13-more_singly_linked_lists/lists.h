6517958176a0f5251267beb8f8776e57df002c93
Nathan <frevnjengamungai@gmail.com>
2022-10-27 03:23:05 -0700

    Prints all elements in a list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef>
 
 /**
  * struct listint_s - singly linked list
@@ -29,4 +30,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 #endif
