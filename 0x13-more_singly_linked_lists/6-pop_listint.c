1fd982c5b9b0331d000df8ff3ad19b4d61f04d1d
Nathan <frevnjengamungai@gmail.com>
2022-10-27 03:46:02 -0700

    Deletes a head node and returns its data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,17 +4,19 @@
  * pop_listint - frees the head node of a list
  * @head: double pointer to the list
  *
- * Return: */
+ * Return: the head node's data
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int i = (*head)->n;
+	int i;
 
 	if (*head == NULL)
 		return (0);
 	temp = *head;
 
 	*head = (*head)->next;
+	i = temp->n;
 	free(temp);
 
 	return (i);
