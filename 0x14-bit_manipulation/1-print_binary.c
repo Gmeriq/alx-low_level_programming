2146360d70eaec31d22577feca9dc09fdcd3d4bd
Nathan <frevnjengamungai@gmail.com>
2022-10-31 08:06:04 -0700

    Prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,18 +8,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	char *b;
-	int i = 0;
-	int len;
-
-	for (; b[i]; i++)
+	if (n > 1)
 	{
-		b[i] = n % 2;
-		n = n / 2;
+		print_binary(n >> 1);
 	}
-	len = strlen(b);
-	for (; b[len]; n--)
-	{
-		_putchar(b[len]);
-	}
+	_putchar((n & 1) + '0');
 }
