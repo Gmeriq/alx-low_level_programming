dbb2ac267e9427e0e7d9ea4dd7fd760c33134b9c
Nathan <frevnjengamungai@gmail.com>
2022-11-03 07:34:59 -0700

    fixing errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,8 +10,7 @@
  */
 int main(int ac, char *argv[])
 {
-	FILE *fp1 = fopen(argv[1], "r");
-        FILE *fp2 = fopen(argv[2], "a");
+	FILE *fp1 = fopen(argv[1], "r"), *fp2 = fopen(argv[2], "a");
 	char buffer[1024];
 	int n;
 
@@ -20,26 +19,22 @@ int main(int ac, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	if (fp1 == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		n = fclose(fp2);
 		exit(98);
 	}
-
 	if (fp2 == NULL)
 	{
 		fclose(fp1);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	while (fgets(buffer, sizeof(buffer), fp1))
 	{
 		dprintf(fileno(fp2), "%s", buffer);
 	}
-
 	n = fclose(fp1);
 	if (n == EOF)
 	{
