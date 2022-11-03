7b1c4678dbe4bc3f143d85eb0415e61c698c8084
Nathan <frevnjengamungai@gmail.com>
2022-11-03 05:02:32 -0700

    clearing whitespaces

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,10 +15,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content == NULL)
 		text_content = "";
-	
+
 	fp = fopen(filename, "w");
 
 	if (fp == NULL)
