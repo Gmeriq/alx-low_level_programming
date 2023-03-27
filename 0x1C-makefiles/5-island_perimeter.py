dddc15c1b8c61865be9a727dce173516de34e935
Nathan Njenga <frevnjengamungai@gmail.com>
2023-03-27 15:04:16 +0300

    error handling

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python
 
 def island_perimeter(grid):
+    p = 0
 
-	p = 0
-
-	for r in range(len(grid)):
-		for c in range(len(grid[0])):
-			if grid[r][c] == 1:
-				if grid[r - 1][c] == 0:
-					p += 1
-				if grid[r][c - 1] == 0:
-					p += 1
-				if grid[r + 1][c] == 0:
-					p += 1
-				if grid[r][c + 1] == 0:
-					p += 1
-
-	return p
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == 1:
+                if grid[r - 1][c] == 0:
+                    p += 1
+                if grid[r][c - 1] == 0:
+                    p += 1
+                if grid[r + 1][c] == 0:
+                    p += 1
+                if grid[r][c + 1] == 0:
+                    p += 1
+    return p
