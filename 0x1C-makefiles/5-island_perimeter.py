2087a3261190239aff484cd2b7d8d8e144100f21
Nathan Njenga <frevnjengamungai@gmail.com>
2023-03-27 14:38:49 +0300

    add 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+def island_perimeter(grid):
+
+	p = 0
+
+	for r in range(len(grid)):
+		for c in range(len(grid[0])):
+			if grid[r][c] == 1:
+				if grid[r - 1][c] == 0:
+					p += 1
+				if grid[r][c - 1] == 0:
+					p += 1
+				if grid[r + 1][c] == 0:
+					p += 1
+				if grid[r][c + 1] == 0:
+					p += 1
+
+	return p
