48cfe47c35df7265245571b257a293cd450fa1c4
Nathan Njenga <frevnjengamungai@gmail.com>
2023-03-28 12:15:45 +0300

    error handling

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,26 +9,20 @@ def island_perimeter(grid):
     """
     p = 0
     r = 0
-    c = 0 
+    c = 0
 
-    if (len(grid) <= 100) and (len(grid[0]) <= 100):
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1 and r ==0:
+    if not grid or not grid[0]:
+        return 0
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == 1:
+                if r == 0 or grid[r - 1][c] == 0:
                     p += 1
-                if grid[r][c] == 1 and r == (len(grid) - 1):
+                if c == 0 or grid[r][c - 1] == 0:
                     p += 1
-                if grid[r][c] == 1 and c == 0:
+                if r == len(grid) - 1 or grid[r + 1][c] == 0:
                     p += 1
-                if grid[r][c] == 1 and c == (len(grid[0]) - 1):
+                if c == len(grid[0]) - 1 or grid[r][c + 1] == 0:
                     p += 1
-                if grid[r][c] == 1:
-                    if grid[r - 1][c] == 0:
-                        p += 1
-                    if grid[r][c - 1] == 0:
-                        p += 1
-                    if grid[r + 1][c] == 0:
-                        p += 1
-                    if grid[r][c + 1] == 0:
-                        p += 1
     return p
